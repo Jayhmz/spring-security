4ac2d82b8beb527215923440bfd3764c02a1fc1b
Jayhmz <Jayhmz10@gmail.com>
2022-05-27 15:55:43 +0100

    just removed white spaces

--- src/main/java/springsecurity/controller/AuthenticationController.java
+++ src/main/java/springsecurity/controller/AuthenticationController.java
@@ -1,12 +1,8 @@
 package springsecurity.controller;
 
 import java.security.Principal;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.PreparedStatementSetter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
